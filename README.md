62ad8d45fb10cf2e709601a69abd2853d59b19cf
Matteo Guerzoni <guerzonim97@gmail.com>
2018-02-16 22:46:41 +0100

    Commenting files

---
+++ README.md
@@ -0,0 +1,21 @@
+# Becautious
+> A minimal webapp to show earthquakes all over the world and sesitize people about those facts.
+> App can be customized, by default are shown the most significant earthquakes in the past 30 days
+
+## Demo
+Available online on [github](https://guertz.github.io/becautious/)
+
+## Folder Structure
+```
+├── data                            
+|   └── ...                       # Image resources
+├── libraries
+|   └── ...                       # Javascript resources
+├── becautious.js                 # Application script
+├── index.html                    # Web app website
+```
+
+## External resources
+[GeoJSON](https://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php)
+[jsonformatter](https://jsonformatter.curiousconcept.com/)
+[p5js](https://p5js.org/)
--- becautious.js
+++ becautious.js
@@ -1,62 +1,58 @@
-var photo; // Variabile che salva la foto
-var diameter1=0;
+// Shared variables
+var photo;      
+var diameter1=0;  
 var diameter2=20;
 var diameter3=40;
 var diameter4=60;
 var diameter5=80;
-var diameter6=100;
+var diameter6=100;// Using 6 diameters is optional
 
-const MAX_DIM = 100; //E il max diameter del cerchio
+const MAX_DIM = 100; // Max dimension of how much each diameters can grow up to
 
-const WIDTH = window.innerWidth;// Il calcolo del largezza del pagina Web
-const OFFSET_WP = 0.944882;// fatto delle prove
-const HEIGHT = window.innerHeight;//Il calcolo del altezza del pagina Web
-const OFFSET_HP = 0.901962;
+const WIDTH = window.innerWidth;
+
+const OFFSET_WP = 0.944882; // In this case:
+                            //    1016 => fixed widow width
+                            //    960  => custom map size with point ok
+                            //
+                            //    constant = 960/1016
+                            //
+
+const HEIGHT = window.innerHeight; 
+const OFFSET_HP = 0.901962; // In this case:
+                            //    510 => fixed window height
+                            //    460 => custom map height with point ok
+                            //
+                            //  constant 460/510
 
 var earthquakes = [];
 
 function setup() {
-  createCanvas(WIDTH, HEIGHT);
-  photo = loadImage("data/worldmap1.png");//background imagine for processing
-  frameRate(20);//the velocita of draw function (drawing)
-  const URL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson";//website of earthquake data
-  loadJSON(URL, initArray); //function to load data
+  createCanvas(WIDTH, HEIGHT); 
+  photo = loadImage("data/worldmap1.png");
+  frameRate(20);  //the velocita about how the draw function is called
+                  //  20 times per second
+
+  const URL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson";
+
+  loadJSON(URL, initArray); 
   
 }
 
+// JSON is a notation to share data between computer in a user readable text format
 function initArray(data) {
+
   for(var i=0; i<data.features.length; i++) {
+
     const item = data.features[i];
+
     var   coords = {
-              x: item.geometry.coordinates[0],
-              y: item.geometry.coordinates[1]
+              lon: item.geometry.coordinates[0],
+              lat: item.geometry.coordinates[1]
           };
-    
+
     earthquakes.push(coords);
   }
-}
-
-function wave( X, Y, diameter){
-  
-  ellipse(X,Y,diameter,diameter);
-  noFill();
-  stroke(204, 102, 0);
-
-}
-
-function earthquake( lat, lon ) {
-  
-  var X=((WIDTH  * OFFSET_WP)/360.0) * (180 + lon);
-  var Y=((HEIGHT * OFFSET_HP)/180.0) * ( 90 - lat);
-  
-  wave(X,Y,diameter1);
-  wave(X,Y,diameter2);
-  wave(X,Y,diameter3);
-  wave(X,Y,diameter4);
-  wave(X,Y,diameter5);
-  wave(X,Y,diameter6);
-  
-
 }
 
 function draw() {
@@ -92,14 +88,40 @@ function draw() {
     diameter6++;
   else
     diameter6=0;
-    
+
   photo.resize(WIDTH, HEIGHT);
+
   image(photo, 0, 0);
   
-  for(var i=0; i<earthquakes.length; i++) { //its a loop 
-    const item = earthquakes[i];//
-    earthquake(item.y, item.x);
+  for(var i=0; i<earthquakes.length; i++) {
+    const earthquake = earthquakes[i];
+
+    drawEarthquake(earthquake.lat, earthquake.lon); 
   }
 
   
 }
+
+function drawEarthquake( lat, lon ) {
+  
+  var X=((WIDTH  * OFFSET_WP)/360.0) * (180 + lon);
+  var Y=((HEIGHT * OFFSET_HP)/180.0) * ( 90 - lat);
+  
+  drawWave(X,Y,diameter1);
+  drawWave(X,Y,diameter2);
+  drawWave(X,Y,diameter3);
+  drawWave(X,Y,diameter4);
+  drawWave(X,Y,diameter5);
+  drawWave(X,Y,diameter6);
+  
+
+}
+
+function drawWave( X, Y, diameter){
+
+  ellipse(X,Y,diameter,diameter);
+  noFill();
+  stroke(204, 102, 0);
+
+}
+
